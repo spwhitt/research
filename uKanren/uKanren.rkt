88a24769161f4664b4bf7bb73a2dc0de71cb19c5
Spencer Whitt <me@swhitt.me>
2021-07-21 01:51:58 -0400

    Implement goals == any all

--- uKanren/uKanren.rkt
+++ uKanren/uKanren.rkt
@@ -1,9 +1,5 @@
 #lang racket
 
-;; Fail and succeed goals
-(define fail '())
-(define (succeed x) (list x))
-
 ;; Variables
 (define (var x)
   (cons '? x))
@@ -12,8 +8,13 @@
   (and (pair? x)
        (eq? (car x) '?)))
 
+(define var= eq?)
+
 (module+ test
-  (require rackunit)
+  ;; 'fail' from rackunit conflicts with my fail defined below
+  (require (only-in rackunit
+    check-true check-false check-not-false
+    check-eq? check-not-eq? check-equal?))
 
   ;; I have decided to use the capital letter convention for variables
   (define A (var 'A))
@@ -46,9 +47,13 @@
   (check-equal? (resolve 'q ex-s) 'q)
   (check-equal? (resolve D ex-s) D))
 
-;; Generate a substitution which equates left with right in context subs.
-;; Pairs and variables are treated as special.
-;; Everything else is treated as an atom.
+;; Parameters:
+;;   left, right: Expressions to unify
+;;     Can contain variables, pairs, and atoms (everything else)
+;;   subs: Variable substitution representing the current context
+;; Returns:
+;;   Success: Substitution unifying left and right
+;;   Failure: #f
 (define (unify left right subs)
   (define vl (resolve left subs))
   (define vr (resolve right subs))
@@ -82,3 +87,47 @@
   (check-equal? (resolve A subs) 'v)
   (check-equal? (resolve B subs) 'v)
   (check-equal? (resolve C subs) 'v))
+
+
+;;; GOALS
+;;; All goals must return a closure of type (substitution -> substitution list)
+
+;; Fail and succeed goals
+;; Empty list indicates no valid substitutions - eg failure
+(define fail (lambda (_) '()))
+(define succeed (lambda (s) (list s)))
+
+;; Unify turned into a goal
+(define (== left right)
+  (lambda (s)
+    (define r (unify left right s))
+    (if r (succeed r) (fail r))))
+
+;; 'Any' goal - succeeds if any argument goal succeeds
+(define (any* . g*)
+  (lambda (s)
+    ;; g* is a list of goals
+    ;; Pass our substitution (s) to each one and append the results
+    ;; Thus, if ANY goal generates a substitution, we will return a substitution
+    (for/fold
+      ([results '()])
+      ([g g*])
+      (append (g s) results))))
+
+(module+ test
+  (check-equal? ((any* (== 'a 'b) (== A A)) empty-s) '(()))
+  (check-equal? ((any* fail fail) empty-s) (fail 'a))
+  (check-true (set=?
+                ((any* (== A 'a) (== A 'b)) empty-s)
+                `(((,A . a)) ((,A . b))))))
+
+;; 'All' goal - succeeds if all argument goals succeed
+(define (all* . g*)
+  (lambda (s)
+    (for/fold
+      ([results (list s)])
+      ([g g*])
+      (apply append (map g results)))))
+
+(module+ test
+  (check-equal? ((all* (== A 'a) (== A 'b)) empty-s) '()))
