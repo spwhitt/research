50b3b055b2428b3ec01d7e09d3307c7ce11a9d01
Spencer Whitt <me@swhitt.me>
2021-07-26 10:24:52 -0400

    Fix appendo
    Execution order matters. In (all...), execute simpler goals first so
    that the result list shrinks prior to the recursive call. This way we
    achieve termination when the result list shrinks to zero.

--- uKanren/uKanren-examples.rkt
+++ uKanren/uKanren-examples.rkt
@@ -16,14 +16,33 @@
   (fresh (h t tb)
     (any
       (all* (== a '()) (== b ab))
-      (all* (conso h t a) (appendo t b tb) (appendo h tb ab)))))
+      (all* (conso h t a) (conso h tb ab) (appendo t b tb)))))
 
-; Some example calls
-; (vars A B C D)
-; (define empty-s '())
-; ((conso 'a 'b A) empty-s)
-; ((conso A 'b (cons 'a 'b)) empty-s)
-; ((caro '(a b c d) A) empty-s)
-; ((cdro (cons 'b 'c) A) empty-s)
-; ((memo 'a '(a b c d)) empty-s)
-; ((appendo '(a ) '(d e f) A) empty-s)
+(module+ test
+  (require rackunit)
+
+  (define-binary-check (check-set=? set=? actual expected))
+
+  (vars A B C D)
+  (check-equal? (run A (conso A  'a (cons 'a 'a))) '(a))
+  (check-equal? (run A (conso A  B (cons B 'a))) '(a))
+  (check-equal? (run A (all* (== A (cons B C)) (== B 'b) (== C 'c))) '((b . c)))
+
+
+  (define empty-s '())
+  (check-equal? (run A (conso 'a 'b A)) '((a . b)))
+  (check-equal? (run A (conso A 'b (cons 'a 'b))) '(a))
+
+  (check-equal? (run A (caro '(a b c d) A)) '(a))
+  (check-equal? (run A (cdro (cons 'b 'c) A)) '(c))
+
+  (check-equal? (run '() (memo 'a '(a b c d))) '(()))
+  (check-set=? (run A (memo A '(a b c d))) '(a b c d))
+
+  (check-equal? (run A (appendo '(a b c) '(d e f) A)) '((a b c d e f)))
+  (check-equal? (run A (appendo A '(d e f) '(a b c d e f))) '((a b c)))
+  (check-equal? (run A (appendo '(a b c) A '(a b c d e f))) '((d e f)))
+  
+  ;; Self Reference
+  ;; (run A (conso A A A))
+  ;; (check-equal? (run A (appendo A '(d e f) A)) '((a b c)))
