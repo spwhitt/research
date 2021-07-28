d63d8e69ca21a0940217370edc474df676dae0cf
Spencer Whitt <me@swhitt.me>
2021-07-28 17:10:59 -0400

    Rewriting: two more approaches to rewrite rules
    Both implement rewrite rules as procedures of type (Term -> [Term]),
    where an empty list signals failure, a singleton list contains the
    result, and a list of multiple elements is currently unused. In the
    future we could imagine implementing term matching in a way which
    returns multiple results, for example matching terms modulo
    commutativity.
    
    Encoding rewrite rules as procedures is convenient when using them, but
    has the disadvantage that they cannot be easily manipulated after
    definition. For example, if we wanted to flip a rewrite rule so it goes
    right to left instead of left to right, there is no obvious way to do
    this when it's encoded as a procedure of type (Term -> [Term])
    
    The second approach implements the rewrite rules using Racket's bundled
    match functionality. This has the disadvantage that we are not able to
    modify the matching algorithm, for example matching modulo commutativity
    as mentioned above. But as an advantage, it provides a lot of free
    functionality.

--- uKanren/rewriting.rkt
+++ uKanren/rewriting.rkt
@@ -18,22 +18,69 @@
 (vars A B C D)
 
 ;; A rule is just a pair
-(define +-commutative
+(define +-comm
   `(rule (+ ,A ,B) (+ ,B ,A)))
 
 (define +-assoc
   `(rule (+ ,A (+ ,B ,C)) (+ (+ ,A ,B) ,C)))
 
 ;; We apply it left to right
+;; '() indicates failure to match
+;; (term) indicates success, term was the result of the match
 (define (apply-rule rule term)
-  (define pattern (cadr rule))
-  (define template (cddr rule))
-  (define substitution (unify pattern term '()))
-  (apply-substitution substitution template))
+  (match-define `(rule ,p1 ,p2) rule)
+  (define substitution (unify p1 term '()))
+  (if substitution
+    (list (apply-substitution substitution p2))
+    '()
+    ))
+
+;; Maybe we want to apply a rule right to left?
+(define (flip rule)
+  (match-define `(rule ,p1 ,p2) rule)
+  `(rule ,p2 ,p1))
 
 ;; EXAMPLES
 (module+ test
   (require rackunit)
 
-  (check-equal? (apply-rule +-commutative math-term) '((+ (+ 10 2) 5)))
-  (check-equal? (apply-rule +-assoc math-term)  '((+ (+ 5 10) 2))))
+  (check-equal? (apply-rule +-comm math-term) '((+ (+ 10 2) 5)))
+  (check-equal? (apply-rule +-assoc math-term)  '((+ (+ 5 10) 2)))
+  (check-equal? (apply-rule (flip +-assoc) '(+ (+ 5 10) 2)) '((+ 5 (+ 10 2))))
+  (check-equal? (apply-rule +-assoc '(* 3 (* 4 5))) '())
+  )
+
+;; It might be convenient to bake application direcly into the rule
+(define-syntax-rule (defrule name p1 p2)
+  (define (name term) (apply-rule `(rule ,p1 ,p2) term)))
+
+(defrule +-comm-f `(+ ,A ,B) `(+ ,B ,A))
+(defrule +-assoc-f `(+ ,A (+ ,B ,C)) `(+ (+ ,A ,B) ,C))
+
+(module+ test
+  (check-equal? (+-comm-f math-term) '((+ (+ 10 2) 5)))
+  (check-equal? (+-assoc-f math-term)  '((+ (+ 5 10) 2)))
+  ;; But now we can't flip the rules
+  ;; (check-equal? ((flip +-assoc-f) '(+ (+ 5 10) 2)) '((+ 5 (+ 10 2))))
+  (check-equal? (+-assoc-f '(* 3 (* 4 5))) '())
+  )
+
+;; Maybe we don't even need to roll our own unification
+;; We could use Racket's built-in match
+;; Using my own implementation still appeals to me more
+;;  1. I have control over it and can extend the matching algorithm
+;;  2. I learn more by doing it myself
+(define-syntax-rule (mrule name p1 p2)
+  (define (name term)
+    (match term
+      [p1 (list p2)]
+      [else (list)])))
+
+(mrule +-comm-m `(+ ,a ,b) `(+ ,b ,a))
+(mrule +-assoc-m `(+ ,a (+ ,b ,c)) `(+ (+ ,a ,b) ,c))
+
+(module+ test
+  (check-equal? (+-comm-m math-term) '((+ (+ 10 2) 5)))
+  (check-equal? (+-assoc-m math-term)  '((+ (+ 5 10) 2)))
+  (check-equal? (+-assoc-m '(* 3 (* 4 5))) '())
+  )
